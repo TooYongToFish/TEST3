c138aefe1ae9419fee27e2054306f89b25fb3a9c
lanou <lan@zhaoyu.local>
2016-07-13 10:51:45 +0800

    2357添加类

---
+++ HELLO/HELLO/Person.h
@@ -0,0 +1,13 @@
+//
+//  Person.h
+//  HELLO
+//
+//  Created by lanou on 16/7/13.
+//  Copyright © 2016年 lanoukeji. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Person : NSObject
+
+@end
---
+++ HELLO/HELLO/Person.m
@@ -0,0 +1,13 @@
+//
+//  Person.m
+//  HELLO
+//
+//  Created by lanou on 16/7/13.
+//  Copyright © 2016年 lanoukeji. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+@end
